073a8c9ebb9ca6074c7f7cf18d26faa067a22781
apoorva srivastava <apoorvasrivastava.14@gmail.com>
2020-10-11 17:17:47 +0530

    added buttons to cals

--- src/Application.js
+++ src/Application.js
@@ -1,43 +1,54 @@
 import React, {Component} from 'react';
-import HighScore from './HighScore';
+import Button from './components/button'
 
 class Application extends Component{
     constructor(props){
         super(props);
         this.state = {
-            count: 0,
-            overTen: false
+            current:'0',
+            previous:[]
         }
     }
 
-    handleClick = () => {
-        this.setState({count: this.state.count+1}) //sets new state for this function
+    reset = () => {
+        this.setState({result: '0'});
     }
 
-    componentDidUpdate(props,state){
-        if(this.state.count >10 && this.state.count != state.count &&!this.state.overTen){
-            this.setState({overTen:true});
-        }
+    addToCurrent = (symbol) => {
+        this.setState({current : this.state.current+symbol});
     }
 
-    resetCount = (e) =>{
-        this.setState({
-            count: 0,
-            overTen: false
-        })
-    }
     render(){ //necessary function
-        let {count} = this.state;
+
+        const buttons = [
+            {symbol: 'C', cols: 3, action: this.reset},
+            {symbol: '/', cols: 1, action: this.addToCurrent},
+            {symbol: '7', cols: 1, action: this.addToCurrent},
+            {symbol: '8', cols: 1, action: this.addToCurrent},
+            {symbol: '9', cols: 1, action: this.addToCurrent},
+            {symbol: 'X', cols: 1, action: this.addToCurrent},
+            {symbol: '4', cols: 1, action: this.addToCurrent},
+            {symbol: '5', cols: 1, action: this.addToCurrent},
+            {symbol: '6', cols: 1, action: this.addToCurrent},
+            {symbol: '-', cols: 1, action: this.addToCurrent},
+            {symbol: '1', cols: 1, action: this.addToCurrent},
+            {symbol: '2', cols: 1, action: this.addToCurrent},
+            {symbol: '3', cols: 1, action: this.addToCurrent},
+            {symbol: '+', cols: 1, action: this.addToCurrent},
+            {symbol: '0', cols: 2, action: this.addToCurrent},
+            {symbol: '.', cols: 1, action: this.addToCurrent},
+            {symbol: '=', cols: 1, action: this.addToCurrent},
+        ];
+
         return (
-                <div>
-                    <h1>You clicked the button {count} times.</h1>
-                    <HighScore 
-                        overTen = {this.state.overTen}
-                        onReset = {this.resetCount}
-                    />
-                    <span>
-                        <button onClick = {(e) => this.handleClick()}>Click me</button>
-                    </span>
+            
+                <div className="App">
+                    <input className="result" type="text" value={this.state.current}/>
+
+                    {buttons.map((btn, i)=>{
+                        return < Button symbol={btn.symbol} cols={btn.cols} action={(symbol) => btn.action} />
+                    })}
+                
                 </div>
                 
             );
